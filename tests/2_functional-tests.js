308cecaabc530e502c84423fdd9d3576bf79f43e
Abdwahab Adekola <adekolaabdwahababisoye@gmail.com>
2024-11-24 07:15:00 +0100

    chai assertions5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -52,7 +52,10 @@ suite('Functional Tests', function () {
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+      assert.equal(res.status,200);
+      assert.equal(res.type,'application/json')
+      assert.equal(res.body.name,'Giovanni')
+      assert.equal(res.body.surname,'da Verrazzano')
 
       done();
     });
@@ -60,11 +63,16 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+const { suiteSetup } = require('mocha');
+Browser.site= 'https://mochai.onrender.com'
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
+const browser = new Browser()
 
-
+suiteSetup(function (done) {
+return browser.visit('/',done)
+});
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
@@ -75,15 +83,27 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill('surname','Colombo');
+      browser.pressButton('submit',()=>{
+        browser.assert.status(200)
+        browser.assert.text('span#name','Cristoforo')
+        browser.assert.text('span#surname','Colombo')
+        browser.assert.elements('span#dates',1)
+      })
 
       done();
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill('surname','Vespucci');
+      browser.pressButton('submit',()=>{
+        browser.assert.status(200)
+        browser.assert.text('span#name','Amerigo')
+        browser.assert.text('span#surname','Vespucci')
+        browser.assert.elements('span#dates',1)
 
       done();
     });
   });
+});
 });
